8bdc47538208e0d8fba6d95b60e6673a8f4dc283
AntonioGiannini2 <agiannini@studenti.apuliadigitalmaker.it>
2022-04-08 13:32:55 +0200

    Progetto Vendita Modificato

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class FunzioniVarie
+    {
+        static int ConvertiStringAInt(string valore)
+        {
+            int valoreInt = 0;
+            bool controlloValore = int.TryParse(valore, out valoreInt);
+            while (controlloValore == false)
+            {
+                Console.WriteLine("Errore: Inserisci un numero intero");
+                valore = Console.ReadLine();
+                controlloValore = int.TryParse(valore, out valoreInt);
+            }
+            return valoreInt;
+        }
+
+    }
+}
--- Vendita/Vendita/Program.cs
+++ Vendita/Vendita/Program.cs
@@ -8,43 +8,50 @@ namespace Vendita
 {
     internal class Program
     {
-       
-        static void Saluta()
+        static Dictionary<string, int> GeneraInventario()
         {
-            Console.WriteLine("Buongiorno!");
+            Dictionary<string, int> inventario = new Dictionary<string, int>();
+            inventario.Add("pane", 2);
+            inventario.Add("carne", 5);
+            inventario.Add("pesce", 7);
+            inventario.Add("formaggio", 3);
+            inventario.Add("pasta", 4);
+            return inventario;
         }
-
-        static Dictionary<string, int> PassaInventario()
+        static void SalutaEMostraProdotti(Dictionary<string, int> inventario)
         {
-            Dictionary<string, int> Inventario = new Dictionary<string, int>();
-            Inventario.Add("pane", 2);
-            Inventario.Add("carne", 5);
-            Inventario.Add("pesce", 7);
-            Inventario.Add("formaggio", 3);
-            Inventario.Add("pasta", 4);
-            return Inventario;
-        }
-
-        static void MostraProdotti()
-        {
-            Dictionary<string, int> inventario = PassaInventario();
-            Console.WriteLine("I prodotti disponili sono:");
+            Console.WriteLine("Buongiorno!\n");
+            Console.WriteLine("I prodotti disponili sono:\n");
             foreach (var prodotto in inventario)
             {
                 Console.WriteLine($"{prodotto.Key}: {prodotto.Value}$");
             }
+            Console.WriteLine("\n");
         }
 
-        static List<int> CreaCarrello()
+        static int ConvertiStringAInt(string valore)
         {
-            Dictionary<string, int> inventario = PassaInventario();
+           int valoreInt = 0; 
+           bool controlloValore = int.TryParse(valore, out valoreInt);
+            while (controlloValore == false)
+            {
+                Console.WriteLine("Errore: Inserisci un numero intero");
+                valore = Console.ReadLine();
+                controlloValore = int.TryParse(valore, out valoreInt);
+            }
+            return valoreInt;
+        }
+
+        static void GestisciOrdine(Dictionary<string, int> inventario, ref int totale)
+        {
+            string ordineCliente = null;
+            string quantità;
+            int quantitàInt;
+            List<int> listaDaPagare = new List<int>();
+
             Console.WriteLine("Quali prodotti vuoi acquistare? " +
                 "(Inserisci il nome del prodotto che vuoi acquistare, inserisci exit quando non vuoi aggiungerne altri)");
-            string ordineCliente = null;
-            string quantità = null;
-            bool controllo = false;
-            int quantitàInt = 0;
-            List<int> totale = new List<int>();
+
             while (ordineCliente != "exit")
             {
                 ordineCliente = Console.ReadLine();
@@ -52,53 +59,39 @@ namespace Vendita
                 {
                     Console.WriteLine($"Quanto/a {ordineCliente} vuoi acquistare?");
                     quantità = Console.ReadLine();
-                    controllo = int.TryParse(quantità, out quantitàInt);
-                    while (controllo == false)                         
-                    {
-                        Console.WriteLine("Errore: Inserisci un numero intero");
-                        quantità = Console.ReadLine();
-                        controllo = int.TryParse(quantità, out quantitàInt);
-                    }
-                    totale.Add(inventario[ordineCliente] * quantitàInt);
-                    Console.WriteLine("Articolo aggiunto correttamente, inseriscine un altro o scrivi exit se non vuoi aggiungerne altri");
+                    quantitàInt = ConvertiStringAInt(quantità);
+                    listaDaPagare.Add(inventario[ordineCliente] * quantitàInt);
+                    Console.WriteLine("Articolo aggiunto correttamente, inseriscine un altro o inserisci exit se non vuoi aggiungerne altri");
                 }
                 else if (ordineCliente != "exit")
                 {
                     Console.WriteLine("Errore: articolo non esiste");
                 }
-                
+                else
+                {
+                    Console.WriteLine("Errore, qualcosa è andato storto");
+                }
             }
-            return totale;
+
+            totale = listaDaPagare.Sum();
         }
 
-        static string SommaProdotti(List<int> totale)
+        static void MostraCostoERingrazia(int totale)
         {
-            return $"Il totale da pagare è: {totale.Sum()}$";
-        }
-
-        static void MostraCostoERingrazia(List<int> totale)
-        {
-            Console.WriteLine(SommaProdotti(totale));
+            Console.WriteLine($"Il totale da pagare è: {totale}$");
             Console.WriteLine("Grazie e arrivederci");
             Console.ReadLine();
         }
 
         static void Main(string[] args)
         {
-           
-
-            Saluta();
-            MostraProdotti();
-            //bool carrelloTerminato;
+            Dictionary<string, int> inventario = GeneraInventario();
             int totale = 0;
-            CreaCarrello(ref totale);
-            /*while (!carrelloTerminato)
-            {
-                AggiungiProdotto(ref carrelloTerminato, ref totale);
-            }*/
 
+            SalutaEMostraProdotti(inventario);
+            
+            GestisciOrdine(inventario, ref totale);
 
-     
             MostraCostoERingrazia(totale);
         }
     }
